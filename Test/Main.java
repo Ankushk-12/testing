41345a361849940f907577760c79f922f9112a6b
Ankushk-12 <121861723+Ankushk-12@users.noreply.github.com>
2024-06-14 13:17:27 +0530

    Update Main.java

--- Test/Main.java
+++ Test/Main.java
@@ -4,6 +4,7 @@ import javax.swing.JFrame;
 public class Main {
     public static void main(String[] args) {
         JCheckBox checkBox = new JCheckBox("Check Box");
+        checkBox.setBounds(100, 100, 150, 50);
         //Frame 
         JFrame frame = new JFrame("Test");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
