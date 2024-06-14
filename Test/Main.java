474c12aa92663ae9e7b8b7ecf1adabdc8e1df2fe
Ankushk-12 <121861723+Ankushk-12@users.noreply.github.com>
2024-06-14 13:07:46 +0530

    Updated the Main.java file

--- Test/Main.java
+++ Test/Main.java
@@ -2,10 +2,12 @@ import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) {
+
+        //Frame 
         JFrame frame = new JFrame("Test");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 400);
         frame.setVisible(true);
-        
+
     }
 }
