be85a7316bab93a5ceb3498074ff25da18c30f87
Ankushk-12 <121861723+Ankushk-12@users.noreply.github.com>
2024-06-14 13:06:31 +0530

    Added the Test folder

---
+++ Test/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Test");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+        
+    }
+}
