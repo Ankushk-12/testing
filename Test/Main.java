018192e118df9b13e175788c76386799c226d0c8
Ankushk-12 <121861723+Ankushk-12@users.noreply.github.com>
2024-06-14 13:13:51 +0530

    Updated the Main.java file

--- Test/Main.java
+++ Test/Main.java
@@ -1,12 +1,15 @@
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) {
-
+        JCheckBox checkBox = new JCheckBox("Check Box");
         //Frame 
         JFrame frame = new JFrame("Test");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 400);
+        frame.setLayout(null);
+        frame.add(checkBox);
         frame.setVisible(true);
 
     }
